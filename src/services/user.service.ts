8c0c9865e8fca06fb2366ec6280d302c2e4cc95e
Sonish <sonishmaharjan1@gmail.com>
2023-01-27 22:59:10 +0545

    fix: update import path to use aliases

--- src/services/user.service.ts
+++ src/services/user.service.ts
@@ -1,8 +1,8 @@
-import { BaseService } from "@core/classes/services/base.service";
-import { GetRepository } from "@core/decorators";
-import { UserEntity } from "@entities/user.entity";
 import { Service } from "typedi";
 import { Repository } from "typeorm";
+import { GetRepository } from "@core/entities";
+import { BaseService } from "@core/services";
+import { UserEntity } from "@entities";
 
 @Service()
 export class UserService extends BaseService<UserEntity> {
