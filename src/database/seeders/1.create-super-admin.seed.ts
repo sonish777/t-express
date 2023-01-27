b4b684a8e7895b6564d79a690d8cdc77b3c7b464
Sonish <sonishmaharjan1@gmail.com>
2023-01-27 22:57:39 +0545

    fix: update import path to use aliases

--- src/database/seeders/1.create-super-admin.seed.ts
+++ src/database/seeders/1.create-super-admin.seed.ts
@@ -1,4 +1,4 @@
-import { UserEntity } from "@entities/user.entity";
+import { UserEntity } from "@entities";
 import { Seeder, Factory } from "typeorm-seeding";
 import { DataSource } from "typeorm/data-source";
 
