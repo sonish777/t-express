2448e33101215b0384b78eaf1db519ddb5ab995c
Sonish <sonishmaharjan1@gmail.com>
2023-01-27 22:59:42 +0545

    fix: update import path to use aliases feat: register static serve provider

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,10 @@
+import { Handler } from "express";
+import path from "path";
+import morgan from "morgan";
 import { Server } from "@core/server";
 import { provideMiddleware } from "@core/utils";
-import morgan from "morgan";
+import { CORSProvider, PassportProvider, StaticServeProvider, StaticServeProviderOptions, ViewEngineOptions, ViewEngineProvider } from "@providers";
 import "@database/connections";
-import { Handler } from "express";
-import { CORSProvider } from "@providers/cors.provider";
-import { ViewEngineOptions, ViewEngineProvider } from "@providers/view-engine.provider";
-import { PassportProvider } from "@providers/passport.provider";
-import { StaticServeProvider, StaticServeProviderOptions } from "@providers/static-serve.provider";
-import path from "path";
 
 function bootstrap() {
     const server = new Server();
