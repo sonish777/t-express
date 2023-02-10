fc33b75c9202b769efa6e346cd3a22fe909f6b10
Sonish <sonishmaharjan1@gmail.com>
2023-02-11 01:29:21 +0545

    fix: fixed lintstaged json config

--- apps/texpress-api/src/index.ts
+++ apps/texpress-api/src/index.ts
@@ -1,34 +1,34 @@
-  import { Handler } from 'express';
-  import path from 'path';
-  import methodOverride from 'method-override';
-  import { Server } from 'core/server';
-  import { provideMiddleware } from 'core/utils';
-  import { PassportProvider } from '@providers';
-  import 'shared/connections';
-  import { ApiExceptionHandler } from '@exceptions/handlers';
-  import * as controllers from '@controllers';
-  import { ServerConfig } from '@configs';
-  import {
-    CORSProvider,
-    StaticServeProvider,
-    StaticServeProviderOptions,
-  } from 'shared/providers';
+import { Handler } from 'express';
+import path from 'path';
+import methodOverride from 'method-override';
+import { Server } from 'core/server';
+import { provideMiddleware } from 'core/utils';
+import { PassportProvider } from '@providers';
+import 'shared/connections';
+import { ApiExceptionHandler } from '@exceptions/handlers';
+import * as controllers from '@controllers';
+import { ServerConfig } from '@configs';
+import {
+  CORSProvider,
+  StaticServeProvider,
+  StaticServeProviderOptions,
+} from 'shared/providers';
 
-  function bootstrap() {
-    const server = new Server(controllers);
-    const middlewares: Handler[] = [methodOverride('_method')];
-    server.startup(Number(ServerConfig.PORT), {
-      middlewares: [...middlewares],
-      middlewareProviders: [
-        CORSProvider,
-        provideMiddleware<StaticServeProviderOptions>(StaticServeProvider, {
-          pathToStaticContents: path.join(__dirname, '../public'),
-          prefix: 'static',
-        }),
-        PassportProvider,
-      ],
-      exceptionHandlers: [new ApiExceptionHandler()],
-    });
-  }
+function bootstrap() {
+  const server = new Server(controllers);
+  const middlewares: Handler[] = [methodOverride('_method')];
+  server.startup(Number(ServerConfig.PORT), {
+    middlewares: [...middlewares],
+    middlewareProviders: [
+      CORSProvider,
+      provideMiddleware<StaticServeProviderOptions>(StaticServeProvider, {
+        pathToStaticContents: path.join(__dirname, '../public'),
+        prefix: 'static',
+      }),
+      PassportProvider,
+    ],
+    exceptionHandlers: [new ApiExceptionHandler()],
+  });
+}
 
-  bootstrap();
+bootstrap();
