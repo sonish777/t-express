1bf83bb49c35cfef4a9af5a9222b297a47fd64f4
Sonish <sonishmaharjan1@gmail.com>
2023-04-01 17:59:51 +0545

    fix: remove unwanted config from api

--- apps/texpress-api/src/configs/server.config.ts
+++ apps/texpress-api/src/configs/server.config.ts
@@ -3,5 +3,4 @@ import config from 'config';
 export const ServerConfig = {
     URL: `${config.get('server.api:host')}:${config.get('server.api:port')}`,
     PORT: process.env.API_PORT || config.get('server.api:port'),
-    API_KEY: config.get<string>('server.api_key'),
 };
