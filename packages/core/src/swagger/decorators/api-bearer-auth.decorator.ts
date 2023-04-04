4a2cfd9d60a31ac9b261b1234403cc994610af21
Sonish <sonishmaharjan1@gmail.com>
2023-04-05 00:18:49 +0545

    fix: downgrade typeorm version to fix entity issue

--- packages/core/src/swagger/decorators/api-bearer-auth.decorator.ts
+++ packages/core/src/swagger/decorators/api-bearer-auth.decorator.ts
@@ -3,7 +3,7 @@ import { SwaggerMetadataKeys } from 'core/utils';
 export function ApiBearerAuth(): MethodDecorator {
     return (target, propKey) => {
         Reflect.defineMetadata(
-            SwaggerMetadataKeys.API_KEY_AUTH,
+            SwaggerMetadataKeys.API_BEARER_AUTH,
             true,
             target.constructor,
             propKey
