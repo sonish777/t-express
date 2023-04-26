0b233ea2d56e062546325d00b2ebf8d72a612489
Sonish <sonishmaharjan1@gmail.com>
2023-04-26 22:45:32 +0545

    Updates

--- component---src-pages-404-js-0ed3c563e006110316ec.js
+++
@@ -1,2 +0,0 @@
-(window.webpackJsonp=window.webpackJsonp||[]).push([[29],{w2l6:function(t,e,n){"use strict";n.r(e);n("r0ML");var i=n("xH0s");e.default=()=>Object(i.c)("div",{style:{display:"flex",alignItems:"center",justifyContent:"center",height:"100vh",width:"100vw",fontSize:32}},"Not Found")}}]);
-//# sourceMappingURL=component---src-pages-404-js-0ed3c563e006110316ec.js.map
---
+++ styles-b5978ee9b1f445bee027.js
@@ -0,0 +1,2 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[35],[]]);
+//# sourceMappingURL=styles-b5978ee9b1f445bee027.js.map
